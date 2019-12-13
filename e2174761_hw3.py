e5351786b203f908192e6c89e5d0b99b44d0e391
ZUMRUD SHUKURLU <e2174761@cenginek>
2019-12-14 00:33:27 +0300

    resolution incomplete

--- e2174761_hw3.py
+++ e2174761_hw3.py
@@ -1,3 +1,35 @@
+# takes a constant (function constant or atomic constant)
+# returns a two-tuple of function name and parameters
+def extract_parameters(literal):
+    if len(literal.split('(')) == 1:
+        # not a good solution but works for now
+        return (literal.rsplit(')', 1)[0], [])
+
+    else:
+        func_name = literal.split('(', 1)[0].rsplit(')', 1)[0]
+        params = literal.split('(', 1)[1].rsplit(')', 1)[0].split(',') 
+
+        return (func_name, [extract_parameters(param) for param in params])
+
+def parse_clause(clause):
+    literals = clause.split('+')
+    result = []
+    for literal in literals:
+         result.append(extract_parameters(literal))
+
+    return result
+
+     
+#def replace(literal, replacement_value):
+#    replaced_literal = (literal[0], replacement_value)
+#    return replaced_literal
+
+#def replace(clause, value):
+#    for elem in clause:
+#        for param in elem[1]:
+
+
+
 def is_tautology(clause):
     #disjuncts = parse_clause(clause)  
     negated = ('',[]) 
@@ -44,32 +76,26 @@ def after_subsumption(clauses):
                 break
 
     return clauses
- 
-def replace(literal, replacement_value):
-    replaced_literal = (literal[0], replacement_value)
-    return replaced_literal
-    
 
-def parse_clause(clause):
-    literals = clause.split('+')
-    result = []
-    for literal in literals:
-         result.append(extract_parameters(literal))
+def resolution(clause1, clause2):
+    res_exists = False
+    for elem1 in clause1:
+        for elem2 in clause2:
+            if elem1[0].startswith('~'):
+                if elem1[0][1:] == elem2[0]:
+                    if elem1[1] == elem2[1]:
+                        res_exists = True
+                        return list(set(clause1.remove(elem1)) | set(clause2.remove(elem2))) 
+                    #else:
+                    #    for param1 in elem1[1]:
+                    #        for param2 in elem2[1]:
+                    #            if param1[1] == [] and param2[1] == []:
+                    #                if param2[0].isupper():
 
-    return result
-    
-# takes a constant (function constant or atomic constant)
-# returns a two-tuple of function name and parameters
-def extract_parameters(literal):
-    if len(literal.split('(')) == 1:
-        # not a good solution but works for now
-        return (literal.rsplit(')', 1)[0], [])
 
-    else:
-        func_name = literal.split('(', 1)[0].rsplit(')', 1)[0]
-        params = literal.split('(', 1)[1].rsplit(')', 1)[0].split(',') 
 
-        return (func_name, [extract_parameters(param) for param in params])
+
+
 
 
 
@@ -78,6 +104,13 @@ def theorem_prover(premises_list, negated_goal):
     #premises_parsed.append(parse_clause(negated_goal))
     premises_parsed = after_tautology_elimination(premises_parsed)
     premises_parsed = after_subsumption(premises_parsed)
+
+    goal_parsed = parse_clause(negated_goal)
+
+
+
+
+
 
 
     return ('no', [])
