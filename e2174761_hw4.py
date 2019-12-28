9869b573d28927668c549246f58229bbeb9ceac8
ZUMRUD SHUKURLU <e2174761@cenginek>
2019-12-28 19:25:17 +0300

    value iteration incomplete

--- e2174761_hw4.py
+++ e2174761_hw4.py
@@ -7,10 +7,10 @@ filepath = sys.argv[1]
 
 # for Value iteration or Q-learning
 alg = ''
-theta = -1
-gamma = -1
-M = -1
-N = -1
+theta = -1  # convergence factor
+gamma = -1 # discount factor
+M = -1 # y-dimension
+N = -1 # x-dimension
 k = -1
 lst_obstacles = []
 l = -1
@@ -80,13 +80,52 @@ with open(filepath) as fp:
         r_regular, r_obstacle, r_pitfall, r_goal = [int(item) for item in fp.readline().split()]
 
 
-board = [['R' for x in range(M)] for y in range(N)]
-for x, y in lst_obstacles:
-    board[x-1][y-1] = 'O'
-for x, y in lst_pitfalls:
-    board[x-1][y-1] = 'P'
+board = [['R' for i in range(M)] for j in range(N)]
+for i, j in lst_obstacles:
+    board[i-1][j-1] = 'O'
+for i, j in lst_pitfalls:
+    board[i-1][j-1] = 'P'
 
 board[goal[0] - 1][goal[1] - 1] = 'G'
 
 for line in board:
     print line, '\n'
+
+rewards = {'R': r_regular, 'O': r_obstacle, 'P': r_pitfall, 'G': r_goal}
+actions = {'N': (1, 0), 'S': (-1, 0), 'E': (0, 1), 'W': (0, -1)}
+
+
+# VALUE ITERATION
+def value_iteration(states, rewards, actions, gamma, theta):
+    utilities = [[0 for i in range(M)] for j in range(N)]   # U
+    utilities_2 = [[0 for i in range(M)] for j in range(N)] # U'
+
+    delta = float('inf') # maximum change in the utility of any state in an iteration
+
+    while delta > theta:
+        delta = 0
+        utilities = utilities_2
+        for i in range(M):
+            for j in range(N):
+                # actions
+                actions_state = actions
+                if i == 0: 
+                    actions_state.pop('S')
+                if j == 0:
+                    actions_state.pop('W')
+                if i == M - 1:
+                    actions_state.pop('N')
+                if j == N - 1:
+                    actions_state.pop('E')
+
+                #max_action 
+                #utilities_2[i][j] = rewards[states[i][j]] + gamma * max([])
+
+
+
+
+
+
+
+
+
