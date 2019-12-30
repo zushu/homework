f8ccb698ee9d8e38b16a4be3219b30877f669c29
ZUMRUD SHUKURLU <e2174761@cenginek>
2019-12-31 02:09:24 +0300

    q-learning complete, not fully tested

--- e2174761_hw4.py
+++ e2174761_hw4.py
@@ -4,8 +4,6 @@ import random
 
 filepath = sys.argv[1]
 
-#fp = open(filepath)
-
 # for Value iteration or Q-learning
 alg = ''
 theta = -1  # convergence factor
@@ -33,7 +31,6 @@ with open(filepath) as fp:
     line = fp.readline().split()[0]
 
     if line == 'V':
-        #print 'here'
         alg = line
         theta = float(fp.readline().split()[0])
         gamma = float(fp.readline().split()[0])
@@ -62,7 +59,10 @@ with open(filepath) as fp:
         alpha = float(fp.readline().split()[0])
         gamma = float(fp.readline().split()[0])
         epsilon = float(fp.readline().split()[0])
-        M, N = [int(item) for item in fp.readline().split()]
+        #M, N = [int(item) for item in fp.readline().split()]
+        tmp = [int(item) for item in fp.readline().split()]
+        M = tmp[0]
+        N = tmp[1]
         k = int(fp.readline().split()[0])
         k_copy = k
         while k_copy > 0:
@@ -89,15 +89,13 @@ for i, j in lst_pitfalls:
 
 board[goal[0] - 1][goal[1] - 1] = 'G'
 
-#for line in board:
-#    print line, '\n'
-
-rewards2 = {'R': r_regular, 'O': r_obstacle, 'P': r_pitfall, 'G': r_goal}
-actions2 = {'N': (1, 0), 'S': (-1, 0), 'E': (0, 1), 'W': (0, -1)}
+rewards = {'R': r_regular, 'O': r_obstacle, 'P': r_pitfall, 'G': r_goal}
+actions = {'N': (1, 0), 'S': (-1, 0), 'E': (0, 1), 'W': (0, -1)}
 
 
 # VALUE ITERATION
-def value_iteration(states, rewards, actions, gamma, theta):
+
+def value_iteration(states=board, rewards=rewards, actions=actions, gamma=gamma, theta=theta):
     utilities = [[0 for i in range(M)] for j in range(N)]   # U
     utilities_2 = [[float('inf') for i in range(M)] for j in range(N)] # U'
     chosen_actions = [['0' for i in range(M)] for j in range(N)]
@@ -123,6 +121,11 @@ def value_iteration(states, rewards, actions, gamma, theta):
 
                 #max_action 
                 next_states_list = {key: utilities[i + a_i][j + a_j] for key, (a_i, a_j) in actions_state.iteritems()}
+                for key, (a_i, a_j) in actions_state.iteritems():
+                    if states[i + a_i][j + a_j] == 'O':
+                        next_states_list.pop(key)
+
+                #if next_states_list != {}:
 
                 Q_a = {key: val for key, val in next_states_list.iteritems()} # just for definition
                 for key, next_state in next_states_list.iteritems():
@@ -150,27 +153,13 @@ def value_iteration(states, rewards, actions, gamma, theta):
 
     return output_list
 
-#print rewards2
-#print 'gamma, theta: ', gamma, theta
-
-#uti, ch_a = value_iteration(board, rewards2, actions2, gamma, theta)
-outp = value_iteration(board, rewards2, actions2, gamma, theta)
-#for i in range(M):
-#    for j in range(N):
-#        print outp[i][j]
-
-with open(sys.argv[2], 'w') as f:
-    for item in outp:
-        for elem in item:
-            print >> f, elem
-
 
 # Q-LEARNING
-def q_learning(current_state, board, rewards, actions, alpha, gamma, epsilon, num_episodes):
+def q_learning(board=board, rewards=rewards, actions=actions, alpha=alpha, gamma=gamma, epsilon=epsilon, num_episodes=num_episodes):
     # state action pair mapping for action rewards
     Q = [[{key: 0 for key, _ in actions.iteritems()} for j in range(M)] for k in range(N)]
     # state action pair mapping for action frequencies
-    N = [[{key: 0 for key, _ in actions.iteritems()} for j in range(M)] for k in range(N)]
+    #N = [[{key: 0 for key, _ in actions.iteritems()} for j in range(M)] for k in range(N)]
 
     rand_state = None
     rand_action = None
@@ -187,31 +176,83 @@ def q_learning(current_state, board, rewards, actions, alpha, gamma, epsilon, nu
         rand_state_index = random.choice(regulars_list)
         rand_state = board[rand_state_index[0]][rand_state_index[1]]
 
-        Q_a = Q[rand_state_index[0]][rand_state_index[1]]
+        while True:
 
-        # choose an action with epsilon greedy approach
-        probability = random.random()
+            Q_a = Q[rand_state_index[0]][rand_state_index[1]]
 
-        if probability <= epsilon:
-            rand_action = random.choice(actions)
-            rand_action_value = Q_a[rand_action] 
+            # choose an action with epsilon greedy approach
+            probability = random.random()
 
-        else:
-            # key
-            rand_action = max(Q_a, key=Q_a.get)
-            # value
-            rand_action_value = Q_a[rand_action]
+            if probability <= epsilon:
+                rand_action = random.choice(list(actions))
+                rand_action_value = Q_a[rand_action] 
+
+            else:
+                # key
+                rand_action = max(Q_a, key=Q_a.get)
+                # value
+                rand_action_value = Q_a[rand_action]
 
 
-        #if 
+            y_coord = rand_state_index[0] + actions[rand_action][0]
+            x_coord = rand_state_index[1] + actions[rand_action][1]
+            outside_board = y_coord < 0 or y_coord >= M or x_coord < 0 or x_coord >= N
+            if outside_board: # or board[y_coord][x_coord] == 'O':
+                # stay in the current cell
+                Q[rand_state_index[0]][rand_state_index[1]][rand_action] = (1 - alpha) * Q_a[rand_action] + alpha * (rewards[board[rand_state_index[0]][rand_state_index[1]]] + gamma * rand_action_value)
+            else:
+                Q_b = Q[y_coord][x_coord]
+                next_action = max(Q_b, key=Q_b.get)
+                next_action_value = Q_b[next_action]
+
+                Q[rand_state_index[0]][rand_state_index[1]][rand_action] = (1 - alpha) * Q_a[rand_action] + alpha * (rewards[board[y_coord][x_coord]] + gamma * next_action_value)
+
+                # next state
+                rand_state = board[y_coord][x_coord]
+                rand_state_index = (y_coord, x_coord)
+
+            if rand_state == 'G':
+                for key in actions.keys():
+                    Q[y_coord][x_coord][key] = rewards[rand_state]
+                break
+            
 
 
         num_episodes = num_episodes - 1
-
     
-    return
+
+    output_list = [[' ' for i in range(M)] for j in range(N)]
+    chosen_actions = [[' ' for i in range(M)] for j in range(N)]
+
+    for i in range(M):
+        for j in range(N):
+            output_action = max(Q[i][j], key=Q[i][j].get)
+            chosen_actions[i][j] = output_action
+
+    for i in range(M):
+        for j in range(N):
+            if chosen_actions[i][j] == 'N':
+                chosen_actions[i][j] = '0'
+            elif chosen_actions[i][j] == 'E':
+                chosen_actions[i][j] = '1'
+            elif chosen_actions[i][j] == 'S':
+                chosen_actions[i][j] = '2'
+            elif chosen_actions[i][j] == 'W':
+                chosen_actions[i][j] = '3'
+            output_list[i][j] = str(j + 1) + ' ' + str(i + 1) + ' ' + chosen_actions[i][j]
 
 
+    return output_list
 
 
+output_list = []
+if alg == 'V':
+    output_list = value_iteration()
+elif alg == 'Q':
+    output_list = q_learning()
+
+with open(sys.argv[2], 'w') as f:
+    for j in range(N):
+        for i in range(M):
+            print >> f, output_list[i][j]
 
